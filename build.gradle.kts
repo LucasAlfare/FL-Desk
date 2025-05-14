1d475b6e61c936b4fc116f361364562af54e2409
LucasAlfare <god-lucas@hotmail.com>
2025-05-14 02:52:32 -0300

    update FL-Base version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,8 @@ repositories {
 }
 
 dependencies {
-  implementation("com.github.LucasAlfare:FL-Base:1.1.1")
-//  implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+  implementation("com.github.LucasAlfare:FL-Base:1.1.2")
   implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.61.0")
-  testImplementation("io.ktor:ktor-server-test-host:3.1.1")
   testImplementation(kotlin("test"))
 }
 
